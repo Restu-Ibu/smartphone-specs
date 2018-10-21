fcaae84ca4c065492049a1d49ef2d78a09e03cd1
emirkipang <emirseptiansorid@gmail.com>
2018-10-21 12:15:26 +0700

    init

---
+++ app/src/main/java/smartphone_specs/restuibu/com/smartphone_specs/MainActivity.java
@@ -0,0 +1,13 @@
+package smartphone_specs.restuibu.com.smartphone_specs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
